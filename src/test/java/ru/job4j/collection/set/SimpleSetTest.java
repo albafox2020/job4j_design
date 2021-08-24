e81d48ff9223ecaec28501bc39d184e89c73d61b
Albina <albafox@yandex.ru>
2021-08-24 23:05:35 +0300

    0. Set, HashSet, TreeSet [#4954 #194148]

--- src/test/java/ru/job4j/collection/set/SimpleSetTest.java
+++ src/test/java/ru/job4j/collection/set/SimpleSetTest.java
@@ -15,6 +15,27 @@ public class SimpleSetTest {
     }
 
     @Test
+    public void whenAddNonNull4elements() {
+        Set<Integer> set = new SimpleSet<>();
+        assertTrue(set.add(1));
+        assertTrue(set.contains(1));
+        assertTrue(set.add(2));
+        assertTrue(set.contains(2));
+        assertTrue(set.add(3));
+        assertTrue(set.contains(3));
+        assertTrue(set.add(4));
+        assertTrue(set.contains(4));
+        assertFalse(set.add(1));
+        assertFalse(set.add(4));
+    }
+
+    @Test
+    public void whenNullElements() {
+        Set<Integer> set = new SimpleSet<>();
+        assertFalse(set.contains(null));
+    }
+
+    @Test
     public void whenAddNull() {
         Set<Integer> set = new SimpleSet<>();
         assertTrue(set.add(null));
